31fd7a73e2bad7b11bb8bc95fb25ef1d30a400ed
dhoegl <firebird@hoeglund.com>
2024-05-15 18:27:10 -0700

    Navbar Test

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
 import Image from "next/image";
+import Footer from "../components/footer";
+import Navbar from "../components/navbar";
 
 export default function Home() {
   return (
+    <>
     <main>
-      <div>Hello</div>
+    <Navbar />
+        <div className="container mt-24 mx-auto px-12 py-4">
+          {/* <HeroSection />
+          <AchievementsSection />
+          <AboutSection />
+          <ProjectSection />
+          <EmailSection /> */}
+        </div>
+        <Footer />
+
     </main>
+    </>
   );
 }
